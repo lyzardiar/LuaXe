92d2c6835f08821d40814e432df5560dc430b2ff
PeyTy <alegorium@gmail.com>
2015-01-26 19:05:35 +0300

    string.lua tostring replaced to Std.string

--- luaxe/boot/string.lua
+++ luaxe/boot/string.lua
@@ -4,8 +4,8 @@ String = {}
 String_String = String
 
 local __StringMeta = getmetatable('')
-function __StringMeta.__add(a,b) 
-  return tostring(a or (a == false and "false") or "null") .. tostring(b or (b == false and "false") or "null")
+function __StringMeta.__add(a,b)
+  return Std.string(a) .. Std.string(b)
 end
 
 __StringMeta.__index = function (str, p)
